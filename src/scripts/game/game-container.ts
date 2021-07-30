764b80cc6a9ab195552405d1e26939c6a4e9f028
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2021-07-30 23:18:03 +0300

    Removed red rectangle

--- src/scripts/game/game-container.ts
+++ src/scripts/game/game-container.ts
@@ -8,11 +8,6 @@ export default class GameContainer extends Phaser.GameObjects.Container {
   }
 
   private init(): void {
-    const graphics: Phaser.GameObjects.Graphics = this.scene.make.graphics({});
-    graphics.fillStyle(0xff0000, 1);
-    graphics.fillRect(0, 0, 100, 100);
-    this.add(graphics);
-
     const sprite: Sprite = new Phaser.GameObjects.Sprite(this.scene, 0, 0, 'phaser-logo');
     this.add(sprite);
 
